1552929c2b6f9568117561ff99970d073b71166d
Érika Santos <43834424+erikasantos612@users.noreply.github.com>
2020-12-22 09:08:57 -0300

    Assistente via voz em Python
    Baseado em meus estados de python e inteligência artificial. O assistente foi criado de forma a tornar as tarefas mais simples e quase totalmente via voz.
    Melhorias na pesquisa wikipédia (correção de bugs), previsão do tempo (uso a API da openweathermap) e para quem quiser conta, é gratuito!
    Pesquisa por CEP, usando pycep.
    Da estrelinha aí!!!!
    (Obs: estou trabalhando continuamente no assistente.

---
+++ assistente.py
@@ -0,0 +1,163 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import os
+import time
+import subprocess
+import wolframalpha
+import json
+import requests
+import email
+import pycep_correios
+import pygame
+from pygame import mixer
+import json
+import urllib.request as urllib2
+import json, requests, sys
+
+
+
+print('Carregando seu assistente')
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def saudacao():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Bom dia")
+        print("Bom dia")
+    elif hour>=12 and hour<18:
+        speak("Boa tarde")
+        print("Boa tarde")
+    else:
+        speak("Boa noite")
+        print("Boa noite")
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Ouvindo")
+        audio=r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='pt-br')
+            print(f"voce disse:{statement}\n")
+
+        except Exception as e:
+            speak("Por favor, repita")
+            return "Nada"
+        return statement
+
+speak("Carregando seu assistente")
+saudacao()
+
+
+if __name__=='__main__':
+
+
+    while True:
+        speak("Como posso ajudar?")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
+
+        if "tchau" in statement or "ok tchau" in statement or "parar" in statement:
+            speak('Encerrando assistente! Até a próxima!')
+            print('Encerrando assistente! Até mais!')
+            break
+
+
+
+        if 'wikipédia' in statement:
+            speak('Pesquisando Wikipedia...')
+            statement =statement.replace("wikipedia", "")
+            lang = wikipedia.set_lang("pt")
+            resultados = wikipedia.summary(statement)
+            speak("De acordo com o wikipedia")
+            print(resultados)
+            speak(resultados)
+            
+        elif 'sobre você' in statement or 'ajuda' in statement:
+            speak('Sou seu assistente Python em português, e ajudo você em tarefas como: pesquisar o wikipedia, abrir o google, youtube, gmail, facebook, consultar um cep, ver as noticias e ver o tempo')     
+
+        elif 'abrir youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            speak("Youtube aberto")
+            time.sleep(5)
+            
+              
+
+        elif 'abrir google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            speak("Google aberto")
+            time.sleep(5)
+
+        elif 'abrir gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            speak("gmail aberto")
+            time.sleep(5)
+
+                    
+        elif 'cep' in statement:
+            speak("Qual o numero do cep")
+            cep = takeCommand()
+            endereco = pycep_correios.get_address_from_cep(cep)
+            speak(endereco)
+            print(endereco)
+            
+            
+        elif 'tempo' in statement:
+            global api_key_openweather
+            global cidade
+            global pais
+            api_key_openweather = "8a3ff12127e3eebd389785d477587fde" 
+            cidade_select = speak("Qual a cidade desejada?")
+            cidade=takeCommand()
+            pais = "br"
+            
+          
+            url_http_req = "http://api.openweathermap.org/data/2.5/weather?q="+cidade+","+pais+"&appid="+api_key_openweather
+            dados_clima = requests.get(url_http_req).json()
+ 
+     
+            temp_atual_kelvin = dados_clima["main"]["temp"]
+            umidade = dados_clima["main"]["humidity"]
+            nebulosidade = dados_clima["clouds"]["all"]
+ 
+            temp_atual_celsius = int(float(temp_atual_kelvin) - 273.15)
+            umidade_atual = float(umidade)
+            nebulosidade = float(nebulosidade)
+            speak("Temperatura atual")
+            speak(temp_atual_celsius)
+            speak("Nebulosidade")
+            speak(nebulosidade)
+            speak("Umidade atual")
+            speak(umidade_atual)
+                      
+
+        elif 'hora' in statement:
+            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Hora certa {strTime}")
+
+        elif "facebook" in statement:
+            webbrowser.open_new_tab("https://www.facebook.com/")
+            speak("facebook aberto")
+
+        elif 'notícias' in statement:
+            noticias = webbrowser.open_new_tab("https://g1.globo.com/")
+            speak('Noticias do G1')
+            time.sleep(6)
+
+        
+       
+
+    time.sleep(3)
